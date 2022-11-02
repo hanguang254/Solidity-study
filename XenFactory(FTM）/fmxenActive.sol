3c0ba67024d690e1a6825a861cd3db3147153275
hanguang254 <725204548@qq.com>
2022-11-02 14:25:19 +0800

    SOlidity

---
+++ XenFactory(FTM）/fmxenActive.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+
+//xen接口
+interface IfmXen {
+    
+    function claimRank(uint256 term) external;
+    function claimMintReward() external;
+    function transfer(address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract fmXenActive{
+    //受益者地址
+    address public owner= 0x6971b57a29764eD7af4A4a1ED7a512Dde9369Ef6;
+
+    //xen 合约地址
+    address public _addr = 0xeF4B763385838FfFc708000f884026B8c0434275 ;
+    
+
+    //构造函数
+    constructor() public{
+        claimxen();
+    }
+
+    function claimxen() private{
+        //xen mint接口
+        IfmXen(_addr).claimRank(3);
+    }
+    
+    //提取xen 发送xen去地址
+    function rewardTransfer() external {
+        IfmXen(_addr).claimMintReward();
+        uint256 banlance = IfmXen(_addr).balanceOf(address(this));
+        IfmXen(_addr).transfer(owner, banlance);
+        selfdestruct(payable(owner));
+    }
+
+}
---
+++ XenFactory(FTM）/fmxenFactory.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "./fmxenActive.sol";
+
+
+interface IfmxenActive{
+    function rewardTransfer() external;
+}
+
+//matic 批量mint  与提取mxen合约
+contract fmxenFactory{
+    // 声明地址管理员
+    address public _owner;
+
+    uint256 public index;
+
+    mapping(uint256 => address) public indexAddress;
+
+    
+    //构造一个管理员 合约创建者
+    constructor()public{
+        _owner =payable(msg.sender);
+    }
+
+
+    // ftm莲批量mint  
+    function mintmxen() public {
+        //批量创建合约
+        for(uint i=0;i<20;i++){
+            fmXenActive cAddress = new fmXenActive{salt: bytes32(i)}();
+            indexAddress[index] = address(cAddress);
+            index = index + 1;
+        }
+        
+    }
+
+    // 批量提取子合约 Mxen
+    function claimXEN() public{
+        require(msg.sender == _owner ,"not owner !");
+        for(uint i = 0;i< index;i++){
+            address stakaddress = indexAddress[i];
+            fmXenActive(stakaddress).rewardTransfer();
+        } 
+    }
+
+}
